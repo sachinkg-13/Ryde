db4c81b097624549a0544b1578b277f7e80a6686
Sachin kumar gupta <sachinkg1309@gmail.com>
2025-02-14 22:27:42 +0530

    add SocketProvider to main application structure for real-time features

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -5,15 +5,19 @@ import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import UserContext from "./context/UserContext.jsx";
 import CaptainContext from "./context/CaptainContext.jsx";
+import { Socket } from "socket.io-client";
+import SocketProvider from "./context/SocketContest.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <CaptainContext>
-      <UserContext>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </UserContext>
-    </CaptainContext>
+    <SocketProvider>
+      <CaptainContext>
+        <UserContext>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </UserContext>
+      </CaptainContext>
+    </SocketProvider>
   </StrictMode>
 );
