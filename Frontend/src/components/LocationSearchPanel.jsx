f1a745b05dc7a315216441857a364d3c8e952248
Sachin kumar gupta <sachinkg1309@gmail.com>
2025-03-26 00:27:19 +0530

    add LocationSearchPanel component for displaying location suggestions

---
+++ Frontend/src/components/LocationSearchPanel.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+const LocationSearchPanel = ({
+  suggestions,
+  setVehiclePanel,
+  setPanelOpen,
+  setPickup,
+  setDestination,
+  activeField,
+}) => {
+  const handleSuggestionClick = (suggestion) => {
+    if (activeField === "pickup") {
+      setPickup(suggestion.description);
+    } else if (activeField === "destination") {
+      setDestination(suggestion.description);
+    }
+    // setVehiclePanel(true);
+    // setPanelOpen(false);
+  };
+
+  return (
+    <div className>
+      {/* {console.log(suggestions)} */}
+      {/* Display fetched suggestions */}
+      {suggestions.map((elem, idx) => (
+        <div
+          key={idx}
+          onClick={() => handleSuggestionClick(elem)}
+          className="flex gap-4 border-2 p-3 border-gray-50 active:border-black rounded-xl items-center my-2 justify-start"
+        >
+          <h2 className="bg-[#eee] h-8 flex items-center justify-center w-12 rounded-full">
+            <i className="ri-map-pin-fill"></i>
+          </h2>
+          <h4 className="font-medium">
+            {elem.description}
+            {/* {console.log("Elem: ", elem.description)} */}
+          </h4>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default LocationSearchPanel;
